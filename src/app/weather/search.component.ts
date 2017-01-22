8095f8ef7d68bb167572096be4fe82617d0e0b24
Haytham Akl <silents.coders@gmail.com>
2017-01-22 14:28:42 -0500

    Add keystroke with subject method

--- src/app/weather/search.component.ts
+++ src/app/weather/search.component.ts
@@ -1,49 +1,49 @@
-import {Component,OnInit} from '@angular/core';
-import {Items} from './items.constructor';
-import {WeatherService} from './service';
-import {Subject} from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
+import { Items } from './items.constructor';
+import { WeatherService } from './service';
+import { Subject } from 'rxjs/Rx';
 
 
 @Component({
-         selector:'SearchComponent',
-         templateUrl:'../weather/SearchComponent.html'
+  selector: 'SearchComponent',
+  templateUrl: '../weather/SearchComponent.html'
 })
 
 export class SearchComponent implements OnInit {
 
-city : string
+  city: string
 
-constructor(private _WeatherService : WeatherService){
+  constructor(private _WeatherService: WeatherService) {
 
-}
-  onSubmit() { 
+  }
+  onSubmit() {
 
-this._WeatherService.SearchWeatherAPI(this.city,"b0ca6bb77b0c0eb7a4647911c29e4483").subscribe(
+    this._WeatherService.SearchWeatherAPI(this.city, "b0ca6bb77b0c0eb7a4647911c29e4483").subscribe(
 
-   data => {
-     const weatherItem  = new Items(data.name,data.weather[0].description,data.main.temp);
-     this._WeatherService.AddWeatherItem(weatherItem);
-   }
+      data => {
+        const weatherItem = new Items(data.name, data.weather[0].description, data.main.temp);
+        this._WeatherService.AddWeatherItem(weatherItem);
+      }
 
-);
-  
-
-    }
-
-    private searchStream = new Subject<string>();
+    );
 
 
-    OnSearch(city:string){
+  }
 
-this.searchStream
-.next(city);
-    }
+  private searchStream = new Subject<string>();
 
-ngOnInit(){
-this.searchStream.subscribe(
-  data => console.log(data)
-)
-}
+
+  OnSearch(city: string) {
+
+    this.searchStream
+      .next(city);
+  }
+
+  ngOnInit() {
+    this.searchStream.subscribe(
+      data => console.log(data)
+    )
+  }
 
 
 }
